4b6032c1bc89ab30e0bbf7ad3a0983c9b8940f94
Sebastian <sebastianbeltran341@gmail.com>
2023-01-29 17:28:03 -0500

    Editar pacientes

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,119 @@
+import { useState, useEffect } from "react"
+import { Error } from "./Error"
+import { generarId } from '../helpers/generarId.js'
+
+export const Formulario = ({ pacientes, setPacientes, paciente }) => {
+
+  const [nombre, setNombre] = useState('')
+  const [propietario, setPropietario] = useState('')
+  const [email, setEmail] = useState('')
+  const [fecha, setFecha] = useState('')
+  const [sintomas, setSintomas] = useState('')
+
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    if (Object.keys(paciente).length > 0) {
+      setNombre(paciente.nombre);
+      setPropietario(paciente.propietario);
+      setEmail(paciente.email);
+      setFecha(paciente.fecha);
+      setSintomas(paciente.sintomas);
+    }
+  }, [paciente])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    // Validar formulario
+    if ([nombre, propietario, email, fecha, sintomas].includes('')) {
+      setError(true)
+      return
+    }
+
+    setError(false)
+
+    // objeto paciente
+    const objPaciente = { nombre, propietario, email, fecha, sintomas, id: generarId() }
+
+    // Copia del objeto + nuevo paciente
+    setPacientes([...pacientes, objPaciente])
+
+    // Reiniciar formulario
+    setNombre('')
+    setPropietario('')
+    setEmail('')
+    setFecha('')
+    setSintomas('')
+  }
+
+  return (
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
+
+      <h2 className="font-black text-3xl text-center mb-10">Seguimiento Paciente</h2>
+      <p className="text-lg mt-5 text-center">Agrega Pacientes y {''} <span className="text-indigo-600 font-bold ">Administralos</span></p>
+
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10" onSubmit={handleSubmit} >
+
+        {error && <Error> Todos los campos son obligatorios</Error>}
+
+        <div className="mb-5">
+          <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
+          <input
+            id="mascota"
+            type="text"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" placeholder="Nombre de las mascota"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+          />
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold">Nombre Propietario</label>
+          <input
+            id="propietario"
+            type="text"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" placeholder="Nombre del propietario"
+            value={propietario}
+            onChange={(e) => setPropietario(e.target.value)}
+          />
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="email" className="block text-gray-700 uppercase font-bold">Email</label>
+          <input
+            id="email"
+            type="email"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" placeholder="Email contacto propietario"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="alta" className="block text-gray-700 uppercase font-bold">Fecha de alta</label>
+          <input
+            id="alta"
+            type="date"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={fecha}
+            onChange={(e) => setFecha(e.target.value)}
+          />
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">Alta</label>
+          <textarea id="sintomas" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" placeholder="Describe los sintomas" value={sintomas}
+            onChange={(e) => setSintomas(e.target.value)}></textarea>
+        </div>
+
+        <input
+          type="submit"
+          value={paciente.id ? 'Editar Paciente' : 'Agregar Paciente'}
+          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all"
+        />
+
+      </form>
+    </div>
+  )
+}
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,26 @@
+import { Paciente } from "./Paciente"
+
+export const ListadoPacientes = ({ pacientes, setPaciente }) => {
+
+  return (
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+      {
+        pacientes && pacientes.length ? (
+          <>
+            <h2 className="font-black text-3xl text-center">ListadoPacientes</h2>
+            <p className="text-lg mt-5 text-center mb-10">Administra tus {''} <span className="text-indigo-600 font-bold ">Pacientes y Citas</span></p>
+            {
+              pacientes.map((paciente) => (
+                <Paciente key={paciente.id} paciente={paciente} setPaciente={setPaciente} />
+              ))
+            }
+          </>
+        ) :
+          <>
+            <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+            <p className="text-lg mt-5 text-center mb-10">Comienza agregando pacientes {''} <span className="text-indigo-600 font-bold ">y apareceran en este lugar</span></p>
+          </>
+      }
+    </div>
+  )
+}
